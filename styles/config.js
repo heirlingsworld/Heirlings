12a60d23828eaac24d4dcfd630c5d62a18cf9bc5
threeofcolons <zaidishangku@gmail.com>
2024-06-28 07:46:57 -0700

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "16KY0NiWkXZqBRCEHuBu7tYRI8e4WeVzp45MqAzTfq5w";
 
 
 /* ------------------------------------------------------------------- */
